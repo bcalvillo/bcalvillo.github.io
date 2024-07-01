2dc933798b5c136dba0ef4d97604e72d8ed1d47c
bcalvillo <benjami.calvillo@upc.edu>
2024-07-01 12:37:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// scripts.js
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("El sitio web de Benjamí Calvillo Melero está listo.");
-
-    const menuToggle = document.getElementById("menu-toggle");
-    const sidebar = document.getElementById("sidebar");
-    const mainContent = document.getElementById("main-content");
-
-    menuToggle.addEventListener("click", function() {
-        if (sidebar.style.left === "-250px") {
-            sidebar.style.left = "0";
-            mainContent.style.marginLeft = "250px";
-        } else {
-            sidebar.style.left = "-250px";
-            mainContent.style.marginLeft = "0";
-        }
-    });
-});
+function toggleMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
